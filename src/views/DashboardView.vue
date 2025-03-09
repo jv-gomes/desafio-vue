9ef9c0c91d54cc3e75f1d58848da5a719a3ad627
João Vitor <jv155gomes@gmail.com>
2025-03-09 13:13:52 -0300

    feat: adicionando os dados da dashboard de temperatura

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {ref, onMounted} from 'vue'
+import { ref, onMounted } from 'vue'
 import axios from "axios"
 // -21.7434194,-43.3583964
 
@@ -8,32 +8,34 @@ interface ApiResponse {
   longitude: number;
 }
 
-
+const loading = ref(true)
 
 // const api = ref<ApiResponse>()
-  const api = ref()
-  const serie = ref([
+const api = ref()
+const serie = ref([
   {
-    name:'temperatura',
-    data:[]
+    name: 'temperatura',
+    data: []
   }
 ])
-  const option = ref({
-  xaxis:{
-    categories:[]
+const option = ref({
+  xaxis: {
+    categories: []
   }
 })
 
 
-  const lineChartSeries = ref([
+const lineChartSeries = ref([
   {
-    name:'hora',
-    data:[]
+    name: 'hora',
+    data: []
   }
 ])
 
 const lineChartOptions = ref()
 
+const units = ref()
+const sunshine = ref()
 
 
 onMounted(async () => {
@@ -43,6 +45,22 @@ onMounted(async () => {
     api.value = response.data
     serie.value[0].data = response.data.hourly.temperature_2m
     option.value.xaxis.categories = response.data.hourly.time
+    console.log(units)
+
+    let hours = Math.floor(response.data.daily.daylight_duration[0]  / 3600);
+    let minutes = Math.floor((response.data.daily.daylight_duration[0]  % 3600) / 60);
+    let seconds = Math.floor(response.data.daily.daylight_duration[0]  % 60);
+
+    units.value = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+
+
+     hours = Math.floor(response.data.daily.sunshine_duration[0] / 3600);
+     minutes = Math.floor((response.data.daily.sunshine_duration[0]  % 3600) / 60);
+     seconds = Math.floor(response.data.daily.sunshine_duration[0]  % 60);
+
+     sunshine.value = `${String(hours).padStart(2, '0')}1:${String(minutes).padStart(2, '0')}2:${String(seconds).padStart(2, '0')}`;
+
+
   } catch (error) {
     console.error("Erro ao buscar dados:", error)
   }
@@ -52,26 +70,93 @@ onMounted(async () => {
 </script>
 
 <template>
-  <main>
     <!-- <v-container> -->
 
+    <v-container class="teste3">
+      <v-row class="mb-6" no-gutters>
+        <v-col>
+          <v-sheet class="pa-2 ma-2 bg-transparent">
+            <v-card title="Nascer do sol">
+                <div v-if="api">
+                  {{  api.daily.sunrise[0].split('T')[1]}}
+                </div>
+                <div v-else>
+                  <v-skeleton-loader :loading="loading" height="200" type="image">
+                  </v-skeleton-loader>
+                </div>
+            </v-card>
+          </v-sheet>
+        </v-col>
 
-    <v-card title="Temperatura" 
-    style="width: 50%; height: 100%; border-radius: 1rem; padding: 0;" 
-     class="custom-card">
-     <div class="test">
-      <div v-if="api">
-      <apexCharts 
-      type="line"
-      :series="serie"
-      :options="option"
-      class="chart"
-       />
-       </div>
-    </div>
-    </v-card>
+        <v-col>
+          <v-sheet class="pa-2 ma-2 bg-transparent">
+            <v-card title="pôr do sol">
+                <div v-if="api">
+                  {{  api.daily.sunset[0].split('T')[1]}}
+                </div>
+                <div v-else>
+                  <v-skeleton-loader :loading="loading" height="200" type="image">
+                  </v-skeleton-loader>
+                </div>
+            </v-card>
+          </v-sheet>
+        </v-col>
 
-<!-- </v-container> -->
+        <v-col>
+          <v-sheet class="pa-2 ma-2 bg-transparent">
+            <v-card title="Temperatura maxima">
+                <div v-if="api">
+                  {{  api.daily.temperature_2m_max[0] }}
+                </div>
+                <div v-else>
+                  <v-skeleton-loader :loading="loading" height="200" type="image">
+                  </v-skeleton-loader>
+                </div>
+            </v-card>
+          </v-sheet>
+        </v-col>
+
+        <v-col>
+          <v-sheet class="pa-2 ma-2 bg-transparent">
+            <v-card title="Temperatura minima">
+                <div v-if="api">
+                  {{ api.daily.temperature_2m_min[0] }}
+                </div>
+                <div v-else>
+                  <v-skeleton-loader :loading="loading" height="200" type="image">
+                  </v-skeleton-loader>
+                </div>
+            </v-card>
+          </v-sheet>
+        </v-col>
+      </v-row>
+
+      <v-row no-gutters class="teste4">
+        <v-col class="h-100">
+          <v-sheet class="pa-2 ma-2 h-50 bg-transparent">
+            <v-card title="Temperatura" class="custom-card">
+              <div class="test">
+                <div v-if="api" class="teste2">
+                  <apexCharts type="line" :series="serie" :options="option" class="chart" width="100%" height="100%" />
+                </div>
+                <div v-else>
+                  <v-skeleton-loader :loading="loading" height="200" type="card">
+                  </v-skeleton-loader>
+                </div>
+              </div>
+            </v-card>
+          </v-sheet>
+        </v-col>
+
+      </v-row>
+    </v-container>
+
+
+
+
+
+
+    <!-- </v-container> -->
     <!-- <div class="flex flex-col gap-16 p-24 items-center bg-gray-700">
       <h2>text </h2>
     <div class="w-1/3 bg-white rounded-xl p-4 " >
@@ -113,20 +198,46 @@ onMounted(async () => {
        />
 
     </div> -->
-  <!-- </div> -->
-  </main>
+    <!-- </div> -->
 </template>
 
 <style>
 
+.custom-card{
+  width: 100%; 
+  height: 100%; 
+  border-radius: 1rem; 
+  padding: 0;
+  /* background-color: blueviolet;  */
+  align-items: center;
 
-.test{
-    width: 97%;
 }
 
+.test {
+  width: 100%;
+  height: 100%;
+  /* background-color: yellow; */
+}
 
+.teste2 {
+  width: 100%;
+  height: 98%;
+  /* background-color: red; */
+}
+.teste3{
+
+  height: 100vh;
+  /* background-color: aqua; */
+
+}
+
+.teste4{
+  height: 70%;
+  /* background-color: blue; */
+}
 .chart {
 
-    height: 6%; /* Faz o gráfico ocupar toda a altura do card */
+  height: 6%;
+  /* Faz o gráfico ocupar toda a altura do card */
 }
 </style>
